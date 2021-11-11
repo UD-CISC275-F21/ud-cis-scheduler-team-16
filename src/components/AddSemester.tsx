501c2c59b797c832c2c813b47dbc738890b9d988
Donovan Messer <donovanmess01@gmail.com>
2021-11-11 09:54:12 -0500

    progress? idk

--- src/components/AddSemester.tsx
+++ src/components/AddSemester.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css";
+import "../App.tsx"
 
 export const customModalSemester = {
     content: {
@@ -17,7 +18,9 @@ export const AddSemester = ({openModal}: {openModal: VoidFunction}): JSX.Element
     return(
         <div>
             <h2 /* add classname here to look nice later*/>Please select how many classes you want to take and input your classes.</h2>
-            <p>How many classes are you going to take during this semester?</p>
+            <p>How many classes are you going to take during this semester? /*Button to determine how many classes */ <br /></p>
+            /*Loop to create a number of courses based on button number (course form to fill out) */
+            /*When all fields are filled out, add semester button adds a semester 
         </div>
     );
 }
---
+++ src/components/getCourseAmt.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export const getCourseAmt = (): number => {
+
+    const[amount, setAmount] = useState(null);
+
+
+    const getAmount(){
+
+    }
+}
